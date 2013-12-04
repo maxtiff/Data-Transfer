f5bbbf103e90825bc790ce903af059aea4c783d3
Travis <maxtiff@users.noreply.github.com>
2013-12-04 16:04:17 -0600

    Create debugger.php

---
+++ debugger.php
@@ -0,0 +1,16 @@
+<?php
+
+require dirname(__FILE__) . '\compare.php';
+
+/**
+ *	FOR DEBUGGING PURPOSES ONLY
+ *
+ */
+$test = new Transfer();
+/*$test->get_username();
+$test->validate_dir();
+$test->count_files();
+$test->count_series();*/
+$test->json_test();
+
+?>
