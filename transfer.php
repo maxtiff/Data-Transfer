b478a611ba4e071d8436a5eed819c6f08deafe1c
Travis <maxtiff@users.noreply.github.com>
2013-12-09 14:10:13 -0600

    Update transfer.php

--- transfer.php
+++ transfer.php
@@ -37,6 +37,13 @@ class Transfer {
 	 */
 	public $file_volume;
 
+	/**
+	 *	Flag to check if files were zipped.
+	 *
+	 *	@var book
+	 *	@access public
+	 */
+	public $zipped;
 
 	/********************************************************************************
 	 * 	AP Server Command Variables 												*
@@ -68,17 +75,18 @@ class Transfer {
 	 ********************************************************************************/
 	const THRESHOLD = 1000; 
 
-
-	public function __construct(/*$dir*/) {
 	/**
 	 * 	Constructor function to initialize class and assign variables.
 	 *
 	 *	@access public
 	 */
+	public function __construct(/*$dir*/) {
+	
 		$this->user_name = strtolower(exec("ECHO %USERNAME%", $output_temp, $return_temp));
 		$this->dir = "C:/Users/$this->user_name/Documents/test_directory/";
 		$this->destination_dir = "/home-ldap/$this->user_name/test_transfer/"; //"/www/fred/data/.../"
 		$this->file_volume= NULL;
+		$this->zipped = False;
 		$this->server = $this->user_name."@ap185.stlouisfed.org";
 		// $this->script_commands = array('sh_file_delete_all_files' => "rm -fr ".$source_directory."* 2>&1",
 		// 				   'sh_file_unzip_file' => "unzip -o ".$tmpfdir.$zip_file." -d ".$source_directory." 2>&1",
@@ -88,23 +96,23 @@ class Transfer {
 		// 				   'sh_file_set_permissions' => "/www/httpd/allow_permissions ".$source_directory." 2>&1");
 	}
 
-
-	public function transfer_series() {
-
-	}
-
-
 	public function validate() {
 
 	}
 
-	public function file_volume_check () {
+
+	public function transfer_series() {
+		//exec($)
+	}
+
 	/**
 	 *	This function takes the volume of the files in the directory from get_directory_size() function; if the volume surpasses the defined threshold, the function will zip
 	 *	the files.
 	 *
 	 *	@access public
 	 */
+	public function file_volume_check () {
+	
 		$this->get_directory_size($this->dir);
 
 		if ($this->file_volume >= THRESHOLD)
@@ -112,7 +120,7 @@ class Transfer {
 			echo "The volume of the files to be transferred exceeds 1000 KBs.\n Zipping files.";
 			$this->zip_files();
 		}
-		elseif (0 < $this->file_volume < THRESHOLD)
+		elseif ($this->file_volume < THRESHOLD || $this->file_volume > 0)
 		{
 			echo "Transfer\n";
 			continue;
@@ -126,14 +134,14 @@ class Transfer {
 
 	}
 
-
+	/**
+	 *	This function measures the file volume of the directory prior to the winzip operation and returns $file_volume in KBs. 
+	 *	
+	 *	@return float
+	 *	@access public
+	 */
 	public function get_directory_size($directory) {
-    /**
-     *	This function measures the file volume of the directory prior to the winzip operation and returns $file_volume in KBs. 
-     *	
-     *	@return float
-     *	@access public
-     */
+    
 	    $dir_size=0;
 	     
 	    if(!$dh=opendir($directory))
@@ -166,13 +174,15 @@ class Transfer {
 	    return $this->file_volume;
 	}
 
-
-	public function zip_files() {
 	/**
 	 *	This function zips the files in the directory if the aggregate volume of the files surpasses the defined threshold of 1000 KBs.
 	 *
 	 *	@access public
 	 */
+	public function zip_files() {
+	
+		$this->zipped = True;
+
 		$zip = new ZipArchive;
 
 		$archive = $zip->open("$this->dir"."test.zip", ZipArchive::CREATE);
@@ -200,10 +210,11 @@ class Transfer {
 
 		            //For the addFile function to work correctly (i.e. to not zip the file with its absolute directory tree), a relative path for each file to be added to the zip file must be provided via the 2nd parameter of the function.
 		            $zip->addFile("$this->dir"."$file", $file);
+		            //unlink("$this->dir"."$file");
 		        }
 			}
-
 			$zip->close();
+
 
 		}
 		else
