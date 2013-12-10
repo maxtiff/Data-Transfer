bb753c5539b960a2aeae38b6d5213676f0555a2b
Travis <maxtiff@users.noreply.github.com>
2013-12-10 15:47:48 -0600

    Update transfer.php
    Modify transfer_series() function

--- transfer.php
+++ transfer.php
@@ -45,7 +45,13 @@ class Transfer {
 	 */
 	public $file_volume;
 
-
+	/**
+	 *	Flag that is turned on if files in directory are compressed into a zip file.	
+	 *
+	 *	@var bool
+	 *	@access public 
+	 */
+	public $zipped;
 
 	/********************************************************************************
 	 * 	AP Server Command Variables 												*
@@ -75,7 +81,7 @@ class Transfer {
 	 *	KB threshold for file volume. If the file volume exceeds this number then 	*
 	 *	all of the files in the directory are compressed into a zip file.			*
 	 ********************************************************************************/
-	const THRESHOLD = 1000;
+	const THRESHOLD = 100;
 
 	/**
 	 * 	Constructor function to initialize class and assign variables.
@@ -88,17 +94,26 @@ class Transfer {
 		$this->dir = "C:/Users/$this->user_name/Documents/test_directory/";
 		$this->ap_file = "C:/Users/$this->user_name/Documents/ap.ppk";
 		$this->zip_file = "test.zip";
+		$this->zipped = false;
 		$this->destination_dir = "/home-ldap/$this->user_name/test_transfer/"; //"/www/fred/data/.../"
 		$this->file_volume= NULL;
 		$this->server = $this->user_name."@ap.stlouisfed.org";
+		$this->ssh_command = "plink -ssh ".$this->server." -i ".$this->ap_file." -m ";
 		$this->ssh_copy = "pscp -i C:/Users/$this->user_name/Documents/ap.ppk ";
-		$this->ssh_transfer = $this->ssh_copy.$this->dir.$this->zip_file." ".$this->server.":".$this->destination_dir." 2>&1";
+		$this->ssh_transfer_zip = $this->ssh_copy.$this->dir.$this->zip_file." ".$this->server.":".$this->destination_dir." 2>&1";
+		$this->ssh_unzip = "unzip ".$this->destination_dir.$this->zip_file." -d ".$this->destination_dir." 2>&1";
+		//$this->ssh_transfer_all = $this->ssh_copy.$this->dir.*." ".$this->server.":".$this->destination_dir." 2>&1";
 		// $this->script_commands = array('sh_file_delete_all_files' => "rm -fr ".$source_directory."* 2>&1",
-		// 				   'sh_file_unzip_file' => "unzip -o ".$tmpfdir.$zip_file." -d ".$source_directory." 2>&1",
+		// 				   'sh_file_unzip_file' => "unzip -o ".$this->destination_dir.$this->zip_file." -d ".$this->destination_dir." 2>&1",
 		// 				   'sh_file_delete_zip_file' => "rm -fr ".$tmpfdir.$zip_file." 2>&1",
 		// 				   'sh_file_count_transferred_files' => "ls -1 ".$source_directory." | wc -l 2>&1",
 		// 				   'sh_file_set_group' => "chown -R ".$user_name.":datadesk ".$source_directory."* 2>&1",
 		// 				   'sh_file_set_permissions' => "/www/httpd/allow_permissions ".$source_directory." 2>&1");
+	}
+
+	public function run() {
+
+
 	}
 
 	public function validate() {
@@ -108,8 +123,69 @@ class Transfer {
 
 	public function transfer_series() {
 		$this->file_volume_check();
-		system($this->ssh_transfer);
+		
+		if ($this->zipped == True) 
+		{
+			system($this->ssh_transfer_zip);
+			system($this->ssh_command.$this->ssh_unzip);
+		}
+		else
+		{
+			
+			if(!$dh=opendir($this->dir))
+	    	{
+	        echo false;
+	        return false;
+	    	}
+
+	    	while($file = readdir($dh))
+			{
+			
+				if($file == "." || $file == "..")
+		        {
+		            echo "Skipping $file\n";
+		            continue;
+		        }
+
+		        if(is_file("$this->dir"."$file"))
+		        {
+		            echo "Transferring $file to AP...\n";
+		            system($this->ssh_copy.$this->dir.$file." ".$this->server.":".$this->destination_dir." 2>&1");
+		        }
+			}
+
+		}
+		
 	}
+
+	/**
+	 *	This function takes the volume of the files in the directory from get_directory_size() function; if the volume surpasses the defined threshold, the function will zip
+	 *	the files.
+	 *
+	 *	@access public
+	 */
+	public function file_volume_check () {
+	
+		$this->get_directory_size($this->dir);
+
+		if ($this->file_volume >= Transfer::THRESHOLD)
+		{
+			echo "The volume of the files to be transferred exceeds 1000 KBs.\n Zipping files.\n";
+			$this->zip_files();
+		}
+		elseif ($this->file_volume < Transfer::THRESHOLD || $this->file_volume > 0)
+		{
+			echo "The volume of the files to be transferred is less than the 1000 KBs threshold.\n";
+		}
+		else
+		{
+			echo "Something terrible has happened";
+			//insert a kill function extended from Compare class
+			exit;
+		}
+
+	}
+
 
 	/**
 	 *	This function measures the file volume of the directory prior to the winzip operation and returns $file_volume in KBs. 
@@ -153,36 +229,7 @@ class Transfer {
 	}
 
 
-	/**
-	 *	This function takes the volume of the files in the directory from get_directory_size() function; if the volume surpasses the defined threshold, the function will zip
-	 *	the files.
-	 *
-	 *	@access public
-	 */
-	public function file_volume_check () {
 	
-		$this->get_directory_size($this->dir);
-
-		if ($this->file_volume >= THRESHOLD)
-		{
-			echo "The volume of the files to be transferred exceeds 1000 KBs.\n Zipping files.";
-			$this->zip_files();
-		}
-		elseif ($this->file_volume < THRESHOLD || $this->file_volume > 0)
-		{
-			echo "Transfer\n";
-			continue;
-		}
-		else
-		{
-			echo "Something terrible has happened";
-			//insert a kill function extended from Compare class
-			exit;
-		}
-
-	}
-
-
 	/**
 	 *	This function zips the files in the directory if the aggregate volume of the files surpasses the defined threshold of 1000 KBs.
 	 *
@@ -203,8 +250,9 @@ class Transfer {
 	        return false;
 	    	}
 
-		    while($file = readdir($dh))
-		    {
+	    	while($file = readdir($dh))
+			{
+			
 				if($file == "." || $file == "..")
 		        {
 		            echo "Skipping $file\n";
@@ -219,6 +267,7 @@ class Transfer {
 		        }
 			}
 			$zip->close();
+			$this->zipped = True;
 		}
 		else
 		{
@@ -228,6 +277,7 @@ class Transfer {
 
 
 	public function compare_transferred() {
+
 	}
 
 }
